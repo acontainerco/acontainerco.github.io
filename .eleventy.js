6a812af549bf643196d7a77bd654a94714c43471
dluman <dluman@allegheny.edu>
2023-07-25 16:06:53 -0400

    Because branching didn't work...

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
-//const { eleventyImagePlugin } = require("@11ty/eleventy-img");
-//const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
-
+const yaml = require("js-yaml");
 const Image = require("@11ty/eleventy-img");
-
 const tagList = require("./utils/collections/tagList.js");
 const markdown = require("markdown-it")(({
     html: true
   }))
+
+markdown.renderer.rules.image = (tokens, idx, options, env, self) => {
+    return html;
+}
 
 module.exports = function(eleventyConfig) {
 
@@ -17,6 +18,15 @@ module.exports = function(eleventyConfig) {
                 return Math.floor(Math.random() * 16).toString(16);
             }
         ).join('');
+    });
+
+    eleventyConfig.addNunjucksGlobal("isMultipleArtists", (artists) => {
+        try{
+        if (artists.length > 1) return true;
+        } catch {
+            // Pass
+        }
+        return false;
     });
 
     eleventyConfig.addFilter('markdown', value => {
@@ -51,4 +61,11 @@ module.exports = function(eleventyConfig) {
         return `<img src = "${data.url}" alt = "${alt}">`;
     });
 
+    eleventyConfig.addDataExtension("yaml", (contents) =>{
+        return yaml.load(contents);
+    });
+
+    eleventyConfig.addDataExtension("yml", (contents) =>{
+        return yaml.load(contents);
+    });
 }
